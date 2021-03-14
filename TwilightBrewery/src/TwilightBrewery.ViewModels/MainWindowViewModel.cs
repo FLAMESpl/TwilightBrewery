27b9cf7b657bb6dc081a84a8822a8c12fac75bc7
Łukasz <lukasz.szafirski@wp.pl>
2021-03-14 11:25:47 +0100

    Give project a structure

---
+++ TwilightBrewery/src/TwilightBrewery.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TwilightBrewery.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
---
+++ TwilightBrewery/src/TwilightBrewery.ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+
+namespace TwilightBrewery.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+        public string Greeting { get; } = "Hello darkness my old friend";
+    }
+}
